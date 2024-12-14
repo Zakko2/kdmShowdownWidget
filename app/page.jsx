c5b106e850973953cc50bc1d2141c6a0785735a3
Zakko2 <el_zakko@yahoo.com>
2024-12-13 22:13:23 -0600

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+// app/page.jsx
+import HitCalculator from '@/components/HitCalculator'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <HitCalculator />
+    </div>
+  )
+}
